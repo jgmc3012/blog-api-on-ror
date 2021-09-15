a24a1187b0b49e2296473b3b06f55d93c55eaaaa
Jesus Millan <millan@asistesi.com>
2021-09-14 23:20:45 -0400

    feat: Create Test for Create and Update with authorization

--- spec/requests/private_posts_spec.rb
+++ spec/requests/private_posts_spec.rb
@@ -8,9 +8,9 @@ end
 
 RSpec.describe 'Posts Authentications', type: :request do
   describe 'GET /posts/{id}' do
+    let!(:user) { create(:user) }
+    let!(:other_user) { create(:user) }
     context 'with a auth valid' do
-        let!(:user) { create(:user) }
-        let!(:other_user) { create(:user) }
         context 'when the user is the owner of the post' do
         end
         
@@ -41,9 +41,65 @@ RSpec.describe 'Posts Authentications', type: :request do
             end
         end
       end
+
     describe 'POST /posts/' do
+      it 'with auth i can create a new post' do
+        post_params = {
+          title: 'Post title',
+          content: 'Post body',
+          published: true,
+        }
+        post '/posts/', params: post_params, headers: auth_headers(user)
+  
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'without auth i cant create a new post' do
+        post_params = {
+          title: 'Post title',
+          content: 'Post body',
+          published: true,
+        }
+        post '/posts/', params: post_params
+  
+        expect(response).to have_http_status(:unauthorized)
+      end
+
     end
+
     describe 'PATCH /posts/{id}/' do
+      context 'with auth' do
+        it 'can update a post where im the author' do
+          article = create(:post, user: user)
+          article_params = {
+            title: 'Post title',
+          } 
+          patch "/posts/#{article.id}/", params: article_params, headers: auth_headers(user)
+    
+          payload = JSON.parse(response.body)
+          expect(response).to have_http_status(:ok)
+          expect(payload['title']).to eq(article_params[:title])
+
+        end
+        it 'cant update a post where im not the author' do
+          article = create(:post, user: other_user)
+          article_params = {
+            title: 'Post title',
+          } 
+          patch "/posts/#{article.id}/", params: article_params, headers: auth_headers(user)
+    
+          expect(response).to have_http_status(:unauthorized)
+        end
+      end
+      it 'without auth i cant update any post' do
+        article =  create(:post)
+        article_params = {
+          title: 'Post title',
+        } 
+        patch "/posts/#{article.id}/", params: article_params
+  
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 end
